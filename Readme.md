faa3663e5fb4bdae7983c5429f1a1a7bcb4201f5
yoko <jr4qpv@yahoo.co.jp>
2020-04-16 11:48:58 +0900

    Doxygenのコメント記述に対応

--- Readme.md
+++ Readme.md
@@ -20,9 +20,39 @@ $ make
 $ make clean
 ```
 
+Doxygenによるドキュメント生成
+-----------------------------
+設定ファイル出力。`Doxyfile`の雛形が作られる。
+
+```
+$ doxygen -g
+```
+
+ドキュメント生成。
+
+```
+$ doxygen
+```
+
+結果は「html/」フォルダに出力されてるので、その中の `index.html` をブラウザで開く。
+
+### Doxygenに関する記事
+下記URLのブログ記事でDoxygenについて紹介している。
+
+* <https://www.yokoweb.net/2020/02/18/macos-doxygen-install/>
+* <https://www.yokoweb.net/2020/02/21/msys2-doxygen-install/>
+* <https://www.yokoweb.net/2020/03/01/windows-doxygen-install/>
+
 梱包ファイル
 -----------------
 
-  * `Readme.md`・・・本説明ファイル
-  * `sample.c`・・・サンプルC言語ソース
-  * `Makefile`・・・サンプルmakeファイル
+* `Readme.md`・・・本説明ファイル
+* `sample.c`・・・サンプルC言語ソース
+* `Makefile`・・・サンプルmakeファイル
+
+来歴
+----
+
+* 2018/11/04 新規作成(By T.Yokobayashi)
+* 2020/04/16 Doxygenのコメント記述に対応
+
--- sample.c
+++ sample.c
@@ -1,6 +1,22 @@
+/**
+ * @file   sample.c
+ * @brief  GCC Test sample program
+ * @author T.Yokobayashi de JR4QPV
+ * @date   2020/04/16
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ *****************************************************************************
+ * @brief  Sample main program
+ * @param[in] argc 引数の個数
+ * @param[in] argv 引数文字列
+ * @return 終了コード
+ * @details
+ *****************************************************************************
+ */
 int main(int argc, char *argv[])
 {
 	int i;
@@ -11,3 +27,12 @@ int main(int argc, char *argv[])
 
 	exit(0);
 }
+
+/**
+ *----------------------------------------------------------------------------
+ * @file   main.c
+ * History
+ * -------
+ * - 2018/11/04 New created.(By T.Yokobayashi)
+ * - 2020/04/16 Write a Doxygen comment.
+ */
